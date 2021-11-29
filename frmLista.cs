388193abc3ee651b87dc88e0ec60b3d6230591d1
ANGELICA ROSARIO VARGAS <angelicarosario24@gmail.com>
2021-11-28 20:33:19 -0400

    Agregar archivos de proyecto.

---
+++ frmLista.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataAcces;
+
+namespace WarehouseProgram
+{
+    public partial class frmLista : Form
+    {
+        public frmLista()
+        {
+            InitializeComponent();
+        }
+        private void frmLista_Load(object sender, EventArgs e)
+        {
+            GetListMaterial();
+        }
+        private void GetListMaterial()
+        {
+            DataMaterial dataMaterial = new DataMaterial();
+
+            dataGridView1.DataSource = dataMaterial.MaterialsList();
+        }
+    }
+}
